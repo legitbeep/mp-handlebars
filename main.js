ab93bbcd448955c0b05c85c84d5b1deafa222a4e
legitbeep <glorytoharsh@gmail.com>
2025-09-26 19:12:34 +0530

    FEAT - update slot reminder template

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ const AFFIRMATION_OBJ = {
 
 const templateData = {
   translated: {
-    path: "\\new-templates\\slot_reminder.handlebars",
+    path: "\\new-templates\\es\\slot_reminder.handlebars",
     output: "\\output\\new-templates\\verify_user.html",
     outputPdf: "\\output\\new-tempaltes\\verify_user.pdf",
     data: {
